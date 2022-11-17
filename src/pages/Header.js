c01adb72514915fa363e2afbcc6c83a4ed8f3af5
sanbleb_bash <sanbleb9@hotmail.com>
2022-11-17 09:27:31 +0200

    continued with meddea queries

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -5,19 +5,11 @@ import Wigets from '../components/wigets';
 const Header = () => {
 	return (
 		<Section>
-			<RightSection>
-				<article className='banner-container'>
-					<img
-						className='banner-image'
-						src='/assets/images/image-web-3-desktop.jpg'
-						alt='Flowers'
-					/>
-				</article>
+			<RightSection className='main-container'>
+				<article className='banner-container'></article>
 				<article className='intro-container'>
 					<h1 className='title'>
-						The Bright{' '}
-						<span style={{ display: 'block', color: 'orange' }}>future of</span>{' '}
-						Web 3.0
+						The Bright <span className='extra'>future of</span> Web 3.0
 					</h1>
 					<div className='intro-text'>
 						<p className='intro-content'>
@@ -43,6 +35,47 @@ const Section = styled.section`
 	gap: 15px;
 	min-height: 60vh;
 	width: 100%;
+
+	@media (max-width: 1110px) {
+		display: flex;
+		flex-direction: column;
+		background: rebeccapurple/0.5;
+		min-height: 90vh;
+		.banner-container {
+			height: 450px;
+			background-image: url('/assets/images/image-web-3-mobile.jpg');
+			background-size: cover;
+			background-repeat: no-repeat;
+			background-position: center;
+		}
+
+		.intro-container {
+			justify-content: space-between;
+			width: 100%;
+		}
+		.intro-text {
+			justify-content: space-between;
+			height: 100%;
+			gap: 3.5rem;
+		}
+
+		.main-container {
+			display: flex;
+			flex-direction: column;
+		}
+	}
+
+	@media (max-width: 900px) {
+		.intro-container {
+			flex-direction: column;
+			justify-content: space-between;
+			width: 100%;
+		}
+		.title,
+		.extra {
+			display: inline;
+		}
+	}
 `;
 const RightSection = styled.section`
 	grid-column: span 2;
@@ -56,6 +89,11 @@ const RightSection = styled.section`
 
 	.banner-container {
 		width: 100%;
+		height: 450px;
+		background-image: url('/assets/images/image-web-3-desktop.jpg');
+		background-size: cover;
+		background-repeat: no-repeat;
+		background-position: center;
 	}
 	.banner-image {
 		width: 100%;
@@ -72,6 +110,11 @@ const RightSection = styled.section`
 			font-family: roboto;
 			margin: 0;
 			padding-right: 4rem;
+		}
+		.title span {
+			display: block;
+			color: coral;
+			text-transform: capitalize;
 		}
 	}
 	.intro-text {
